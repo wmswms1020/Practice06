c4b0b22aece0157ad27ca097d31e2caade53fb87
wmswm <wmswm@211.222.232.230>
2022-05-08 21:32:55 +0900

    해결

--- src/com/javaex/ex01/Customer.java
+++ src/com/javaex/ex01/Customer.java
@@ -1,7 +1,41 @@
 package com.javaex.ex01;
 
-public class Customer extends Person{
-	
+public class Customer extends Person {
 
-	
+	// 필드
+	private int cNo;
+	private int point;
+
+	// 생성자
+	public Customer() {
+	}
+
+	public Customer(String name, String hp, int cNo, int point) {
+		super(name,hp);
+		this.cNo = cNo;
+		this.point = point;
+	}
+
+	// 메소드 gs
+	public int getcNo() {
+		return cNo;
+	}
+
+	public void setcNo(int cNo) {
+		this.cNo = cNo;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	// 메소드 일반
+	public void showInfo() {
+		System.out.println("#이름:" +getName()+", #핸드폰:"+getHp()+"#고객번호:" + cNo + ", #포인트점수:" + point);
+	}
+
 }
--- src/com/javaex/ex01/Person.java
+++ src/com/javaex/ex01/Person.java
@@ -1,7 +1,44 @@
 package com.javaex.ex01;
 
 public class Person {
+
+	// 필드
+	private String name;
+	private String hp;
+
+	// 생성자
+	public Person() {
+	}
+
+	public Person(String name, String hp) {
+		this.name = name;
+		this.hp = hp;
+	}
+
+	// 메소드 gs
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	// 메소드, 일반
+	
+	public void showInfo() {
+		System.out.println("#이름:"+name+", #핸드폰: "+hp);
+	}
 	
 	
 	
+
 }
